7fb108a5b563e26e9847493ff59199c30323e05e
Bret Shroats <bshroats5@gmail.com>
2023-10-30 22:51:01 -0400

    caesar cipher assignment

--- cipher.py
+++ cipher.py
@@ -1,1 +1,20 @@
 # add your code here
+def caesar_cipher(text):
+    result = ""
+    for char in text:
+        if char.isalpha():  # Check if the character is a letter
+            ascii_offset = ord('a' if char.islower() else 'A')  # Get ASCII offset for lowercase or uppercase
+            shifted_char = chr((ord(char) - ascii_offset + 5) % 26 + ascii_offset)  # Apply Caesar cipher with shift of 5
+            result += shifted_char
+        else:
+            result += char  # Keep non-alphabetic characters unchanged
+    return result
+
+# Ask user for input
+plain_text = input("Please enter a sentence: ")
+
+# Encrypt the input using Caesar cipher with a right shift of 5
+encrypted_text = caesar_cipher(plain_text)
+
+# Print the encrypted text
+print(f"The encrypted sentence is: {encrypted_text}")
