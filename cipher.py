378c6bd86fcfc1246208a01135ebfa2061ea3a7b
Bret Shroats <bshroats5@gmail.com>
2023-11-08 19:20:02 -0500

    Another attempt

--- cipher.py
+++ cipher.py
@@ -1,13 +1,13 @@
 def caesar_cipher(text):
-  result = ""
-  for char in text:
-      if char.isalpha(): # Check if the character is a letter
-          ascii_offset = ord('A') if char.isupper() else ord('a') # Get ASCII offset for uppercase or lowercase
-          shifted_char = chr((ord(char) - ascii_offset + 5) % 26 + ascii_offset) # Apply Caesar cipher with shift of 5
-          result += shifted_char
-      else:
-          result += char # Keep non-alphabetic characters unchanged
-  return result
+   result = ""
+   for char in text:
+       if char.isalpha(): # Check if the character is a letter
+           ascii_offset = ord('A') if char.isupper() else ord('a') # Get ASCII offset for uppercase or lowercase
+           shifted_char = chr((ord(char) - ascii_offset + 5) % 26 + ascii_offset) # Apply Caesar cipher with shift of 5
+           result += shifted_char
+       else:
+           result += char # Keep non-alphabetic characters unchanged
+   return result
 
 # Ask user for input
 plain_text = input("Please enter a sentence:")
